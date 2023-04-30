631d3c4587af33d087f453f1a67050a4abe6a47e
Json Ajax <sw.houssam@gmail.com>
2023-04-30 23:54:17 +0200

    try to solve the problem

--- main.c
+++ main.c
@@ -3,6 +3,87 @@
 
 #include "lib/Stack.h"
 
+int sherching (char *a,char c_start,char c_end){
+    int endis;
+    for(int i=0;a[i]!='\0';i++){
+
+        if (a[i]==c_end)
+            return i;
+        
+        if ((a[i]==')')&&(a[i]!=c_end))
+            return 0;
+        
+        if ((a[i]==']')&&(a[i]!=c_end))
+            return 0;
+
+        if ((a[i]=='}')&&(a[i]!=c_end))
+            return 0;
+        
+        if (a[i]=='('){
+            endis=sherching(&a[i+1],'(',')');
+            if ((endis)==0)
+                return 0;
+            i=endis;
+        }
+
+        else if (a[i]=='['){
+            endis=sherching(&a[i+1],'[',']');
+            if ((endis)==0)
+                return 0;
+            i=endis;
+        }
+
+        else if (a[i]=='{'){
+            endis=sherching(&a[i+1],'{','}');
+            if ((endis)==0)
+                return 0;
+            i=endis;
+        }
+
+        else if (a[i]==c_start){
+            endis=sherching (&a[i+1],c_start,c_end);
+            if ((endis)==0)
+                return 0;
+            i=endis;
+        }
+
+    }
+    return 0;
+}
+int test (char *a){
+    int endis;
+    for(int i=0;a[i]!='\0';i++){
+
+        if(a[i]==')')
+        return 0;
+        if(a[i]==']')
+        return 0;
+        if(a[i]=='}')
+        return 0;
+
+        if(a[i]=='('){
+            endis=sherching(&a[i+1],'(',')');
+            if ((endis)==0)
+                return 0;
+            i=endis;
+        }
+        else if(a[i]=='['){
+            endis = sherching(&a[i+1],'[',']');
+            if ((endis)==0)
+                return 0;
+            i=endis;
+        }
+        else if(a[i]=='{'){
+            endis=sherching(&a[i+1],'{','}');
+            if ((endis)==0)
+                return 0;
+            i=endis;
+        }
+    }
+    return 1;
+}
+
+
 int main(){
     
     char a[] = "[qskldjf qsfk qskldfj jfqsdfkl]"; //true
@@ -12,7 +93,7 @@ int main(){
     char e[] = "((())()"; //false
     
     
-    
+    /*
     Stack* s = createStack(10);
 
     printStack(s);
@@ -44,6 +125,7 @@ int main(){
     
 
     deleteStack(s);
-
+    */
+    printf("a = %d\nb = %d\nc = %d\nd = %d\ne = %d\n",test(a),test(b),test(c),test(d),test(e));
     return 0;
 }
